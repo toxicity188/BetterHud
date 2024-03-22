30892a7837564edbcfcc5713179933663aab9d36
toxicity188 <angryko@naver.com>
2024-03-22 11:48:56 +0900

    Fixes negative font.

--- dist/src/main/kotlin/kr/toxicity/hud/util/Adventures.kt
+++ dist/src/main/kotlin/kr/toxicity/hud/util/Adventures.kt
@@ -40,7 +40,7 @@ val NEW_LAYER
     get() = if (VERSION.version <= 18) EMPTY_WIDTH_COMPONENT else WidthComponent(Component.text().content(0xC0000.parseChar()).font(SPACE_KEY), 0)
 
 private val LEGACY_NEGATIVE_ONE_SPACE_COMPONENT = WidthComponent(Component.text().content((0xFFC00 - 1).parseChar()).font(LEGACY_SPACE_KEY), 0)
-private val CURRENT_NEGATIVE_ONE_SPACE_COMPONENT = WidthComponent(Component.text().content((0xFFC00 - 1).parseChar()).font(LEGACY_SPACE_KEY), 0)
+private val CURRENT_NEGATIVE_ONE_SPACE_COMPONENT = WidthComponent(Component.text().content((0xD0000 - 1).parseChar()).font(SPACE_KEY), 0)
 val NEGATIVE_ONE_SPACE_COMPONENT
     get() = if (VERSION.version <= 18) {
         CURRENT_NEGATIVE_ONE_SPACE_COMPONENT
