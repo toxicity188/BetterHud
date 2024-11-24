57e236c6e3aacdcc02759f443fc86c60b620886a
toxicity <angryko@naver.com>
2024-11-24 21:30:23 +0900

    Bumps version.

--- bootstrap/bukkit/build.gradle.kts
+++ bootstrap/bukkit/build.gradle.kts
@@ -17,9 +17,9 @@ dependencies {
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.4.0-SNAPSHOT")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.1.0-SNAPSHOT")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
-    compileOnly("com.github.SkriptLang:Skript:2.9.3")
-    compileOnly("net.skinsrestorer:skinsrestorer-api:15.4.3")
-    compileOnly("com.alessiodp.parties:parties-bukkit:3.2.15")
+    compileOnly("com.github.SkriptLang:Skript:2.9.4")
+    compileOnly("net.skinsrestorer:skinsrestorer-api:15.4.4")
+    compileOnly("com.alessiodp.parties:parties-bukkit:3.2.16")
     compileOnly("com.nexomc:nexo:0.1.0-dev.0")
     compileOnly("team.unnamed:creative-api:1.7.3")
 }
