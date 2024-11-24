4f96835da382fcb03e548494bad7025788ccd547
toxicity <angryko@naver.com>
2024-11-24 21:37:23 +0900

    Fix armor placeholder.

--- bootstrap/fabric/src/main/kotlin/kr/toxicity/hud/bootstrap/fabric/util/Fabrics.kt
+++ bootstrap/fabric/src/main/kotlin/kr/toxicity/hud/bootstrap/fabric/util/Fabrics.kt
@@ -3,7 +3,6 @@ package kr.toxicity.hud.bootstrap.fabric.util
 import kr.toxicity.hud.api.player.HudPlayer
 import net.fabricmc.loader.api.FabricLoader
 import net.luckperms.api.LuckPermsProvider
-import net.minecraft.core.component.DataComponents
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.ai.attributes.Attributes
 
@@ -22,12 +21,9 @@ val HudPlayer.fabricPlayer
 
 val ServerPlayer.armor
     get(): Double {
-        val attribute = Attributes.ARMOR.unwrapKey().orElse(null) ?: return 0.0
-        return armorSlots.sumOf {
-            it.get(DataComponents.ATTRIBUTE_MODIFIERS)?.modifiers?.sumOf { modifier ->
-                if (modifier.attribute.`is`(attribute)) modifier.modifier.amount else 0.0
-            } ?: 0.0
-        }
+        return attributes.getInstance(Attributes.ARMOR)?.modifiers?.sumOf { modifier ->
+            modifier.amount
+        } ?: 0.0
     }
 
 fun ServerPlayer.hasPermission(perm: String) = PERMISSION_GETTER(this, perm)
