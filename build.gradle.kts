ab6b58d0377b920d9c640e14900771dd96bcbe5d
Yang Seong Mo <angryko@naver.com>
2024-12-05 17:56:22 +0900

    Optimize and fix text renderer (#62)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     `java-library`
     kotlin("jvm") version "2.1.0"
     id("io.github.goooler.shadow") version "8.1.8"
-    id("io.papermc.paperweight.userdev") version "1.7.4" apply false
+    id("io.papermc.paperweight.userdev") version "1.7.6" apply false
     id("xyz.jpenilla.run-paper") version "2.3.1"
     id("org.jetbrains.dokka") version "2.0.0-Beta"
     id("io.papermc.hangar-publish-plugin") version "0.1.2"
